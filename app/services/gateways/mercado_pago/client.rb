711ee7247cb7bf89ff8971f28cd56d8c943f3460
Arly Moura <arlymoura@gmail.com>
2024-12-16 16:51:12 -0300

    feat: Add payment processing and retrieval methods to MercadoPago client

--- app/services/gateways/mercado_pago/client.rb
+++ app/services/gateways/mercado_pago/client.rb
@@ -12,6 +12,14 @@ module Gateways
         Gateways::MercadoPago::Customers::Create.call(email, conn)
       end
 
+      def process_payment(attempt)
+        Gateways::MercadoPago::Payments::Create.call(conn, data: data)
+      end
+
+      def find_payment(attempt)
+        Gateways::MercadoPago::Payments::Get.call(conn, id: attempt.gateway_reference).call
+      end
+
       private
 
       def conn
